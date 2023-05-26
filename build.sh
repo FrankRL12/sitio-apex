5e4d422764dafd78dbeb2483f169addab1477e60
FrankRL12 <ramirezlujanojosefrancisco4@gmail.com>
2023-05-26 09:36:14 -0600

    commit

--- build.sh
+++ build.sh
@@ -6,4 +6,12 @@ pip install -r requirements.txt
 
 
 python manage.py collectstatic --no-input
+python manage.py makemigrations
 python manage.py migrate
+
+# Variables de entorno para el superusuario
+SUPERUSER_USERNAME="admin"
+SUPERUSER_EMAIL="ramirezlujanojosefrancisco4@gmail.com"
+SUPERUSER_PASSWORD="+t@ckc&12"
+
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('$SUPERUSER_USERNAME', '$SUPERUSER_EMAIL', '$SUPERUSER_PASSWORD')" | python manage.py shell
